2b219710f15d02f667a8806c0a1712808f6d2311
Javi111003 <128992433+Javi111003@users.noreply.github.com>
2025-02-01 18:40:34 -0500

    feat: Entire Domain represented and sytem states to handle work and tasks assignations and final state

--- src/Model/Task.hs
+++ src/Model/Task.hs
@@ -1,15 +1,18 @@
 module Model.Task (Task(..)) where
 
-import Model.Date
+import Data.Time.Calendar (Day)
 import Model.Resource
+
+data PriorityType = Low | Medium | High deriving (Show, Eq)
 
 data Task = Task {
     taskId :: Int,
     description :: String,
-    stimatedTime :: Int, --Tiempo Estimado en Horas
-    startDate :: Date, --Fecha en la que se debe iniciar la tarea
-    endDate :: Date, --La tarea debe finalizar en esta fecha o antes
-    priority :: Int, --Indica que tan importante es la tarea, si dos tareas comienzan el mismo dia, se debe priorizar la de mayor prioridad
+    estimatedTime :: Int, --Tiempo Estimado en Horas
+    startDate :: Day, --Fecha en la que se debe iniciar la tarea
+    deadline :: Day, --La tarea debe finalizar en esta fecha o antes
+    priority :: PriorityType, --Indica que tan importante es la tarea, si dos tareas comienzan el mismo dia, se debe priorizar la de mayor prioridad
     requiredSkills :: [String],
-    requiredResources :: [Resource]
-} deriving (Show)
+    requiredResources :: [(Resource, Int)], --Cantidad de cada recurso necesario para realizar la tarea
+    dependencies :: [Task] --Tareas que deben finalizar antes de que esta pueda comenzar(pendiente)
+} deriving (Show, Eq)
